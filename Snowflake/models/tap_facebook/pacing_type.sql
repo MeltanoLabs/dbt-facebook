3c9cd1cdc86c909b0b8cf3827cd23537d7825cd6
NeilGorman104 <neil@ryan-miranda.com>
2023-04-25 12:46:50 -0400

    Pacing_type column matching

--- Snowflake/models/tap_facebook/pacing_type.sql
+++ Snowflake/models/tap_facebook/pacing_type.sql
@@ -5,10 +5,10 @@
 }}
 
  
-SELECT ACCOUNT_ID,
-       ID as ADSET_ID,
-       TO_TIMESTAMP_NTZ(CREATED_TIME, 'YYYY-MM-DD"T"HH24:MI:SSTZHTZM') as AD_SET_CREATED_TIME,
-       NAME,
-       TO_TIMESTAMP_NTZ(UPDATED_TIME, 'YYYY-MM-DD"T"HH24:MI:SSTZHTZM') as AD_SET_UPDATED_TIME
+SELECT ID as ADSET_ID,
+       TO_TIMESTAMP_NTZ(UPDATED_TIME, 'YYYY-MM-DD"T"HH24:MI:SSTZHTZM') as AD_SET_UPDATED_TIME,
+       /* TODO: Find and add column 'INDEX'*/
+       NAME
+       
 
 FROM {{ source('tap_facebook', 'adsets') }} as pacing_type
