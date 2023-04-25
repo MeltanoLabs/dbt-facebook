74a60e47f3e8a51afdb5875584830025dd0d88b1
NeilGorman104 <neil@ryan-miranda.com>
2023-04-25 11:48:45 -0400

    hotfix: reach_frequency rename

--- Snowflake/models/tap_facebook/reach_frequency.sql
+++ Snowflake/models/tap_facebook/reach_frequency.sql
@@ -7,4 +7,4 @@
 SELECT ACCOUNT_ID
       
       /*TODO: Add Columns: COUNTRY_CODE, MIN_REACH_LIMITS, MAX_DAYS_TO_FINISH, MIN_CAMPAIGN_DURATION, MAX_CAMPAIGN_DURATION */
-FROM {{ source('tap_facebook', 'adsinsights') }} as meltano_reach_frequency
+FROM {{ source('tap_facebook', 'adsinsights') }} as reach_frequency
