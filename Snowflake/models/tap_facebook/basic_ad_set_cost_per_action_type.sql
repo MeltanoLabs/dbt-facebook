4f12522d397cfcba06d9e8ce41bdd9e13f4f2795
Dhrruv <dhrruv@Dhrruvs-MBP.bbrouter>
2023-05-09 14:07:37 +0530

    added basic ad set cost per action type dbt model

---
+++ Snowflake/models/tap_facebook/basic_ad_set_cost_per_action_type.sql
@@ -0,0 +1,39 @@
+{{
+   config(
+     materialized='view'
+   )
+}}
+
+SELECT ADSET_ID,
+       DATE,
+       ACTION_TYPE,
+       VALUE,
+       _7_D_CLICK,
+       _SDC_BATCHED_AT,
+       INLINE::float as INLINE,
+       _1_D_VIEW
+       /* INDEX column*/
+
+FROM (SELECT ADSET_ID,
+             DATE, 
+             REPLACE(COST_PER_ACTION:"action_type", '"','')::varchar as ACTION_TYPE,
+             REPLACE(COST_PER_ACTION:"value", '"','')::float as VALUE,
+             REPLACE(COST_PER_ACTION:"7d_click", '"','')::float as _7_D_CLICK,
+             _SDC_BATCHED_AT,
+             REPLACE(COST_PER_ACTION:"1d_view", '"', '')::float as _1_D_VIEW,
+             CASE WHEN _7_D_CLICK IS NULL AND _1_D_VIEW IS NULL THEN VALUE ELSE NULL END as INLINE
+
+      FROM (SELECT ADSET_ID,
+                   COST_PER_ACTION_TYPE_COLUMN.value as COST_PER_ACTION,
+                   DATE,
+                   _SDC_BATCHED_AT
+       
+            FROM (SELECT ADSET_ID,
+                         CAST(DATE_START as date) as DATE,
+                         ACCOUNT_ID,
+                         PARSE_JSON(COST_PER_ACTION_TYPE) as COST_PER_ACTION_COLUMN,
+                         _SDC_BATCHED_AT
+
+                  FROM {{ source('tap_facebook', 'adsinsights') }}) meltano_basic_ad_set_cost_per_action_type,
+                  lateral flatten(input=>COST_PER_ACTION_COLUMN) COST_PER_ACTION_TYPE_COLUMN))
+
