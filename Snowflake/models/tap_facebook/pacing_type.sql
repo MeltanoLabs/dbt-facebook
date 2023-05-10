38142fa7044b4d00ba0f9924e2fbeaa866bf659c
eryanRM <78859138+eryanRM@users.noreply.github.com>
2023-05-10 13:49:36 -0400

    Update pacing_type.sql fivetran index to NULL

--- Snowflake/models/tap_facebook/pacing_type.sql
+++ Snowflake/models/tap_facebook/pacing_type.sql
@@ -6,7 +6,7 @@
  
 SELECT ID as AD_SET_ID,
        TO_TIMESTAMP_NTZ(UPDATED_TIME, 'YYYY-MM-DD"T"HH24:MI:SSTZHTZM') as AD_SET_UPDATED_TIME,
-       0 as INDEX, /* Add value for INDEX*/
+       NULL as INDEX, /* Add value for INDEX*/
        ARRAY_TO_STRING(PARSE_JSON(PACING_TYPE), '') as NAME,
        _SDC_BATCHED_AT
 
