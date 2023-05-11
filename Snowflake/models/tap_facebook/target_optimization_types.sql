f60d1d86cf888de44d6688c0fa44e377cb26de15
NeilGorman104 <neil@ryan-miranda.com>
2023-05-11 17:31:22 -0400

    remove extra comment

--- Snowflake/models/tap_facebook/target_optimization_types.sql
+++ Snowflake/models/tap_facebook/target_optimization_types.sql
@@ -4,5 +4,4 @@ SELECT ID,
       --VALUE
       --KEY
        _SDC_BATCHED_AT
-       /* TODO: Add missing columns 'Value', 'Key' */
 FROM {{ source('tap_facebook', 'adsets') }} as meltano_ad_set_target_optimization
