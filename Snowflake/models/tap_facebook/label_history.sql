a64255eb895a0433d195e9e4d0a3fc52ae5a6c22
Dhrruv <dhrruv@Dhrruvs-MBP.bbrouter>
2023-05-11 14:42:11 +0530

    removed subqueries from custom audience history, label history, ad recommendation dbt models

--- Snowflake/models/tap_facebook/label_history.sql
+++ Snowflake/models/tap_facebook/label_history.sql
@@ -3,37 +3,12 @@
      materialized='view'
    )
 }}
-
-{% set json_column_query %}
-select distinct json.key as column_name
-
-FROM {{ source('tap_facebook', 'adlabels')}},
-
-lateral flatten(input=>ACCOUNT) json
-{% endset %}
  
-{% set account_results = run_query(json_column_query) %}
-
-{% if execute %}
-{# Return the first column #}
-{% set account_list = account_results.columns[0].values() %}
-{% else %}
-{% set account_list = [] %}
-{% endif %}
- 
-SELECT ADLABELS_ID as ID,
-       UPDATED_TIME,
-       ACCOUNT_ID::number as ACCOUNT_ID,
-       CREATED_TIME, 
-       NAME      
-FROM (SELECT ID as ADLABELS_ID,
-             TO_TIMESTAMP_NTZ(CREATED_TIME, 'YYYY-MM-DD"T"HH24:MI:SSTZHTZM') as CREATED_TIME,
-             TO_TIMESTAMP_NTZ(UPDATED_TIME, 'YYYY-MM-DD"T"HH24:MI:SSTZHTZM') as UPDATED_TIME,
-             NAME,
-             
-             {% for column_name in account_list %}
-             ACCOUNT:{{column_name}}::varchar as {{column_name}}{%- if not loop.last %},{% endif -%}
-             {% endfor %},
+SELECT ID,
+       TO_TIMESTAMP_NTZ(UPDATED_TIME, 'YYYY-MM-DD"T"HH24:MI:SSTZHTZM') as UPDATED_TIME,
+       ACCOUNT:account_id::number as ACCOUNT_ID,
+       TO_TIMESTAMP_NTZ(CREATED_TIME, 'YYYY-MM-DD"T"HH24:MI:SSTZHTZM') as CREATED_TIME,
+       NAME,
        _SDC_BATCHED_AT       
 
-       FROM {{ source('tap_facebook', 'adlabels') }} as label_history)
+FROM {{ source('tap_facebook', 'adlabels') }} as label_history
