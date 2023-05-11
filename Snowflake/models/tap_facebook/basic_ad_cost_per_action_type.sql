1f590eeffc9e6d993c223276631144e0a959ec87
Dhrruv <dhrruv@Dhrruvs-MBP.bbrouter>
2023-05-11 17:41:25 +0530

    removed subqueries and dnested basic ad actions

--- Snowflake/models/tap_facebook/basic_ad_cost_per_action_type.sql
+++ Snowflake/models/tap_facebook/basic_ad_cost_per_action_type.sql
@@ -5,35 +5,15 @@
 }}
 
 SELECT AD_ID,
-       DATE,
-       ACTION_TYPE,
-       VALUE,
-       _7_D_CLICK,
+       CAST(DATE_START as date) as DATE,
+       REPLACE(PARSE_JSON(value):"action_type", '"','')::varchar as ACTION_TYPE,
+       REPLACE(PARSE_JSON(value):"value", '"','')::float as VALUE,
+       REPLACE(PARSE_JSON(value):"7d_click", '"','')::float as _7_D_CLICK,
+       INDEX-1 as INDEX,
        _SDC_BATCHED_AT,
-       INLINE::float as INLINE,
-       _1_D_VIEW
-       /* INDEX column*/
+       CASE WHEN _7_D_CLICK IS NULL AND REPLACE(PARSE_JSON(value):"1d_view", '"', '') IS NULL THEN REPLACE(PARSE_JSON(value):"value", '"','') ELSE NULL END::float as INLINE,
+       REPLACE(PARSE_JSON(value):"1d_view", '"', '')::float as _1_D_VIEW
 
-FROM (SELECT AD_ID,
-             DATE, 
-             REPLACE(COST_PER_ACTION:"action_type", '"','')::varchar as ACTION_TYPE,
-             REPLACE(COST_PER_ACTION:"value", '"','')::float as VALUE,
-             REPLACE(COST_PER_ACTION:"7d_click", '"','')::float as _7_D_CLICK,
-             _SDC_BATCHED_AT,
-             REPLACE(COST_PER_ACTION:"1d_view", '"', '')::float as _1_D_VIEW,
-             CASE WHEN _7_D_CLICK IS NULL AND _1_D_VIEW IS NULL THEN VALUE ELSE NULL END as INLINE
-
-      FROM (SELECT AD_ID,
-                   COST_PER_ACTION_TYPE_COLUMN.value as COST_PER_ACTION,
-                   DATE,
-                   _SDC_BATCHED_AT
-       
-            FROM (SELECT AD_ID,
-                         CAST(DATE_START as date) as DATE,
-                         ACCOUNT_ID,
-                         PARSE_JSON(COST_PER_ACTION_TYPE) as COST_PER_ACTION_COLUMN,
-                         _SDC_BATCHED_AT
-
-                  FROM {{ source('tap_facebook', 'adsinsights') }}) meltano_cost_per_action_type,
-                  lateral flatten(input=>COST_PER_ACTION_COLUMN) COST_PER_ACTION_TYPE_COLUMN))
+FROM {{ source('tap_facebook', 'adsinsights') }} meltano_cost_per_action_type,
+lateral split_to_table(input=>ARRAY_TO_STRING(PARSE_JSON(COST_PER_ACTION_TYPE), '|'), '|') COST_PER_ACTION_TYPE_COLUMN
 
---
+++ Snowflake/models/tap_facebook/basic_ad_set_actions.sql
@@ -0,0 +1,19 @@
+{{
+   config(
+     materialized='view'
+   )
+}}
+
+SELECT ADSET_ID,
+       CAST(DATE_START as date) as DATE,
+       REPLACE(PARSE_JSON(value):"action_type", '"','')::varchar as ACTION_TYPE,
+       ROUND(REPLACE(PARSE_JSON(value):"value", '"',''))::float as VALUE,
+       ROUND(REPLACE(PARSE_JSON(value):"7d_click", '"',''))::float as _7_D_CLICK,
+       INDEX-1 as INDEX,
+       _SDC_BATCHED_AT,
+       CASE WHEN _7_D_CLICK IS NOT NULL AND REPLACE(PARSE_JSON(value):"1d_view", '"', '') IS NOT NULL THEN ROUND(REPLACE(PARSE_JSON(value):"value", '"','')) ELSE NULL END::float as INLINE,
+       ROUND(REPLACE(PARSE_JSON(value):"1d_view", '"', ''))::float as _1_D_VIEW
+
+FROM {{ source('tap_facebook', 'adsinsights') }} meltano_basic_ad_set_actions,
+lateral split_to_table(input=>ARRAY_TO_STRING(PARSE_JSON(COST_PER_ACTION_TYPE), '|'), '|') COST_PER_ACTION_TYPE_COLUMN
+
