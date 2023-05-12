432204953690aa3333551a1016a7fa3a3c391eba
Dhrruv <dhrruv@Dhrruvs-MBP.bbrouter>
2023-05-12 15:41:42 +0530

    updated account history, pacinf type, and lookalike origin dbt models

--- Snowflake/models/tap_facebook/pacing_type.sql
+++ Snowflake/models/tap_facebook/pacing_type.sql
@@ -1,8 +1,8 @@
 SELECT id as ad_set_id,
        TO_TIMESTAMP_NTZ(updated_time, 'YYYY-MM-DD"T"HH24:MI:SSTZHTZM') as ad_set_updated_time,
-       NULL as index, --Add data for INDEX
-       ARRAY_TO_STRING(PARSE_JSON(pacing_type), '') as name,
+       index-1 as index,
+       ARRAY_TO_STRING(PARSE_JSON(pacing_type), '|') as name,
        _sdc_batched_at
 
-
-FROM {{ source('tap_facebook', 'adsets') }}
+FROM {{ source('tap_facebook', 'adsets') }} as meltano_pacing_type,
+lateral split_to_table(input=>ARRAY_TO_STRING(PARSE_JSON(pacing_type), '|'), '|') PACING_TYPE_COLUMN
